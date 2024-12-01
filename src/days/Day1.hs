a8d134ff9f7467bfc68a879fafabe0d7534fa079
DKarandikar <daniel.karandikar@zoho.com>
2024-12-01 19:16:45 +0000

    refactor: tidy day 1 part 1

--- src/days/Day1.hs
+++ src/days/Day1.hs
@@ -1,30 +1,33 @@
 module Day1 where
 
-import Data.List ( sort )
+import Data.List ( sort, singleton )
 import Data.Hashable(Hashable)
 import Data.Map(Map, fromListWith, lookup)
 import Data.Maybe(fromMaybe)
 
 day1part1 :: IO ()
-day1part1 = do
-    input <- readFile "input/day1.txt"
-    print $ doDay1Part1 input
+day1part1 = print . doDay1Part1 =<< readFile "input/day1.txt"
 
 day1part2 :: IO ()
-day1part2 = do
-    input <- readFile "input/day1.txt"
-    print $ doDay1Part2 input
+day1part2 = print . doDay1Part2 =<< readFile "input/day2.txt"
 
 doDay1Part1 :: String -> Int
-doDay1Part1 s = do
-    let list1 = map (\x -> read (head (words x)) :: Int) (lines s) ++ [0]
-    let list2 = map (\x -> read (head (reverse (words x))) :: Int) (lines s) ++ [0]
-    sum $ map Day1.compare (zip (sort list1) (sort list2))
+doDay1Part1 s = sum $ map Day1.compare $ zipSorted $ processInput s
 
+processInput :: String -> [(Int, Int)]
+processInput s = foldr lineAggregate [] (lines s)
+
+lineAggregate :: String -> [(Int, Int)] -> [(Int, Int)]
+lineAggregate = (++) . singleton . processLine
+
+processLine :: String -> (Int, Int)
+processLine s = (read (head (words s)) :: Int, read (last (words s)) :: Int)
+
+zipSorted:: [(Int, Int)] -> [(Int, Int)]
+zipSorted x = (zip (sort (map fst x)) (sort (map snd x)))
 
 compare :: (Int, Int) -> Int
 compare (x, y) = abs (x - y)
-
 
 doDay1Part2 :: String -> Int
 doDay1Part2 s = do
