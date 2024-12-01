aa3e22175c2b06fcfd711f4d1fc8017abf1c81cb
DKarandikar <daniel.karandikar@zoho.com>
2024-12-01 19:35:39 +0000

    refactor: tidy day 1 more

--- src/days/Day1.hs
+++ src/days/Day1.hs
@@ -1,7 +1,6 @@
 module Day1 where
 
 import Data.List ( sort, singleton )
-import Data.Hashable(Hashable)
 import Data.Map(Map, fromListWith, lookup)
 import Data.Maybe(fromMaybe)
 
@@ -9,10 +8,10 @@ day1part1 :: IO ()
 day1part1 = print . doDay1Part1 =<< readFile "input/day1.txt"
 
 day1part2 :: IO ()
-day1part2 = print . doDay1Part2 =<< readFile "input/day2.txt"
+day1part2 = print . doDay1Part2 =<< readFile "input/day1.txt"
 
 doDay1Part1 :: String -> Int
-doDay1Part1 s = sum $ map Day1.compare $ zipSorted $ processInput s
+doDay1Part1 s = sum $ map diffTuple $ zipSorted $ processInput s
 
 processInput :: String -> [(Int, Int)]
 processInput s = foldr lineAggregate [] (lines s)
@@ -26,19 +25,16 @@ processLine s = (read (head (words s)) :: Int, read (last (words s)) :: Int)
 zipSorted:: [(Int, Int)] -> [(Int, Int)]
 zipSorted x = (zip (sort (map fst x)) (sort (map snd x)))
 
-compare :: (Int, Int) -> Int
-compare (x, y) = abs (x - y)
+diffTuple :: (Int, Int) -> Int
+diffTuple (x, y) = abs (x - y)
 
 doDay1Part2 :: String -> Int
-doDay1Part2 s = do
-    let list1 = map (\x -> read (head (words x)) :: Int) (lines s) ++ [0]
-    let list2 = map (\x -> read (head (reverse (words x))) :: Int) (lines s) ++ [0]
-    
-    let counts = toCounter list2
+doDay1Part2 = sum . getSimilarityArray . processInput
 
-    sum $ map (simScore counts) list1
+getSimilarityArray :: [(Int, Int)] -> [Int]
+getSimilarityArray x = map (simScore (toCounter (map snd x))) (map fst x)
 
-toCounter :: (Hashable a, Ord a) => [a] -> Map a Int
+toCounter :: [Int] -> Map Int Int
 toCounter = fromListWith (+) . (map (\x -> (x, 1)))
 
 simScore :: Map Int Int -> Int -> Int
