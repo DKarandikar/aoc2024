b9607a3398e1ae43864253252f28171271b36795
DKarandikar <daniel.karandikar@zoho.com>
2024-12-02 12:40:59 +0000

    feat: implement day 2 part 1

--- src/days/Day2.hs
+++ src/days/Day2.hs
@@ -1,7 +1,37 @@
 module Day2 where
 
+type Report = [Int]
+
+data Step = Inc | Dec | Fail deriving (Eq)
+
 day2part1 :: IO ()
-day2part1 = print $ "day2part1"
+day2part1 = print . doDay2Part1 =<< readFile "input/day2.txt"
 
 day2part2 :: IO ()
 day2part2 = print $ "day2part2"
+
+doDay2Part1 :: String -> Int
+doDay2Part1 s = sum $ map isReportValid (parseInput s)
+
+parseInput :: String -> [Report]
+parseInput s = map parseLine (lines s)
+
+parseLine :: String -> Report
+parseLine s = map (\x -> read x :: Int) (words s)
+
+isReportValid:: Report -> Int
+isReportValid r = fromEnum $ isStepsValid $  map toStep $ window r
+
+isStepsValid :: [Step] -> Bool
+isStepsValid x = or [all (==Inc) x, all (==Dec) x]
+
+toStep :: (Int, Int) -> Step
+toStep (x, y)  
+    | x > y && x - y < 4 = Inc
+    | x < y && y - x < 4 = Dec
+    | otherwise = Fail
+
+
+window :: [Int] -> [(Int, Int)]
+window [] = []
+window xs = zip xs (tail xs)
---
+++ test/Day2Spec.hs
@@ -0,0 +1,14 @@
+module Day2Spec where
+
+import Test.Hspec
+import Day2
+
+spec :: Spec
+spec = do
+  describe "2" $ do
+    it "doDay2Part1 handles simple example" $ do
+        testInput <- readFile "test/input/day2.txt"
+        doDay2Part1 testInput `shouldBe` 2
+
+    it "window handles simple example" $ do
+      window [1, 2, 3, 4] `shouldBe` [(1, 2), (2, 3), (3, 4)]
