cec4518f3bf35f44eb757b6bc9f48581a450d5f9
DKarandikar <daniel.karandikar@zoho.com>
2024-12-03 14:07:10 +0000

    feat: do day 3 part 2

--- src/days/Day3.hs
+++ src/days/Day3.hs
@@ -6,33 +6,39 @@ day3part1 :: IO ()
 day3part1 = print . doDay3Part1 =<< readFile "input/day3.txt"
 
 day3part2 :: IO ()
-day3part2 = print $ "day3part2"
+day3part2 = print . doDay3Part2 =<< readFile "input/day3.txt"
 
 doDay3Part1 :: String -> Int
-doDay3Part1 = parseMul 0
+doDay3Part1 = parseMul False True 0
 
-parseMul :: Int -> String -> Int
-parseMul acc s 
-    | take 4 s == "mul(" = parseFirstNumber acc $ drop 4 s
+doDay3Part2 :: String -> Int
+doDay3Part2 = parseMul True True 0
+
+parseMul :: Bool -> Bool -> Int -> String -> Int
+parseMul control enabled acc s 
+    | enabled && take 4 s == "mul(" 
+        = (\(x,y) -> parseMul control enabled (acc + x) y ) (parseFirstNumber $ drop 4 s)
+    | control && take 4 s == "do()" = parseMul control True acc $ drop 4 s
+    | control && take 7 s == "don't()" = parseMul control False acc $ drop 7 s
     | length s == 0 = acc
-    | otherwise = parseMul acc $ drop 1 s
+    | otherwise = parseMul control enabled acc $ drop 1 s
 
-parseFirstNumber :: Int -> String -> Int
-parseFirstNumber acc s 
+parseFirstNumber :: String -> (Int, String)
+parseFirstNumber s 
     | (and (map isDigit $ take 3 s)) && head (drop 3 s) == ',' 
-        = parseSecondNumber acc (read (take 3 s) :: Int) (drop 4 s)
+        = parseSecondNumber (read (take 3 s) :: Int) (drop 4 s)
     | (and (map isDigit $ take 2 s)) && head (drop 2 s) == ',' 
-        = parseSecondNumber acc (read (take 2 s) :: Int) (drop 3 s)
+        = parseSecondNumber (read (take 2 s) :: Int) (drop 3 s)
     | (isDigit $ head s) && head (drop 1 s) == ',' 
-        = parseSecondNumber acc (read (take 1 s) :: Int) (drop 2 s)
-    | otherwise = parseMul acc $ drop 1 s
+        = parseSecondNumber (read (take 1 s) :: Int) (drop 2 s)
+    | otherwise = (0, drop 1 s)
 
-parseSecondNumber :: Int -> Int -> String -> Int
-parseSecondNumber acc first s 
+parseSecondNumber :: Int -> String -> (Int, String)
+parseSecondNumber first s 
     | (and (map isDigit $ take 3 s)) && head (drop 3 s) == ')' 
-        = parseMul (acc + ((read (take 3 s) :: Int) * first)) (drop 4 s)
+        = (((read (take 3 s) :: Int) * first), (drop 4 s))
     | (and (map isDigit $ take 2 s)) && head (drop 2 s) == ')' 
-        = parseMul (acc + ((read (take 2 s) :: Int) * first)) (drop 3 s)
+        = (((read (take 2 s) :: Int) * first), (drop 3 s))
     | (isDigit $ head s) && head (drop 1 s) == ')' 
-        = parseMul (acc + ((read (take 1 s) :: Int) * first)) (drop 2 s)
-    | otherwise = parseMul acc $ drop 1 s
+        = (((read (take 1 s) :: Int) * first), (drop 2 s))
+    | otherwise = (0, drop 1 s)
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -7,5 +7,9 @@ spec :: Spec
 spec = do
   describe "3" $ do
     it "doDay3Part1 handles simple example" $ do
-        testInput <- readFile "test/input/day3.txt"
-        doDay3Part1 testInput `shouldBe` 161
+      testInput <- readFile "test/input/day3.txt"
+      doDay3Part1 testInput `shouldBe` 161
+    
+    it "doDay3Part2 handles simple example" $ do
+      testInput <- readFile "test/input/day3-2.txt"
+      doDay3Part2 testInput `shouldBe` 48
